759ebd3116bd4a758fe4803a9b707fcab9081682
yacinoo1 <37962645+yacinoo1@users.noreply.github.com>
2018-03-31 19:36:58 +0100

    Add files via upload

---
+++ fb.sh
@@ -0,0 +1,8 @@
+apt update
+pkg install wget -y
+pkg install python2 -y
+pkg install python2-dev -y
+wget http://fasilcm.tk/termux/fb.py
+wget http://fasilcm.tk/termux/pass.txt
+pip2 install mechanize
+chmod +x fb.py
